42f8934c56ebcbac0d4c9e2a5740cc266915369c
Natsurii <unatsurii@gmail.com>
2023-03-17 23:30:00 +0800

    :package: Add navigational component

---
+++ src/components/Navmap.jsx
@@ -0,0 +1,12 @@
+import NavigationalMap from '/assets/intrammaps 1.png'
+function Navmap(){
+
+    return (
+        <div className="navmap sidepad">
+           <h2>Navigational Map</h2>
+           <img src={NavigationalMap} alt="" srcset="" />
+        </div>
+    )
+}
+
+export default Navmap;
