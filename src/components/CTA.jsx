345bb5086666d17f8de9d3d1d756418eef5a00c4
Natsurii <unatsurii@gmail.com>
2023-03-19 11:45:04 +0800

    :package: added CTA component

---
+++ src/components/CTA.jsx
@@ -0,0 +1,11 @@
+function CTA(){
+
+    return (
+        <div className="cta">
+            <h2>Step back in time and discover the stories hidden within Intramuros' walls.</h2>
+            <button>Book Now!</button>
+        </div>
+        )
+    }
+
+export default CTA
