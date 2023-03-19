ce0483762206299b210cb5d9afd6674ac91c293c
Natsurii <unatsurii@gmail.com>
2023-03-20 07:18:29 +0800

    :pencil2: add testimonial data

---
+++ src/data/testimonialdata.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+        message: "Intramuros was a thrilling experience! The city's walls and gates offer stunning views of the surrounding city and the Manila Bay, and the attractions such as the dungeons and torture chambers in Fort Santiago gave me goosebumps. I also enjoyed the fun activities such as riding the kalesa, and exploring the city on foot. It's a unique and exciting destination that's not to be missed!",
+        user: "Jack Wilson",
+        image: "/assets/jack_wilson.jpg"
+    },
+    {
+        message: "Intramuros is a vibrant and lively district that perfectly blends old-world charm with modern amenities. I loved exploring the narrow streets, and the museums and galleries that showcase the Philippines' rich cultural heritage. The locals were friendly and welcoming, and the food was fantastic.",
+        user: "Maria Rodriguez",
+        image: "/assets/maria_rodriguez.jpg"
+    },
+    {
+        message: "Intramuros is a treasure trove of Philippine history. I was amazed by the well-preserved colonial-era architecture and landmarks such as Fort Santiago and San Agustin Church. Walking along the walls and gates gave me a sense of nostalgia and wonder. It's a must-visit destination for anyone interested in Philippine history.",
+        user: "John Smith",
+        image: "/assets/john_smith.jpg"
+    },
+    
+]
