d51548c5497ab31e686f46656417acad588e2daa
Natsurii <unatsurii@gmail.com>
2023-03-20 22:02:48 +0800

    :sparkles: Add links to the buttons

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -3,7 +3,8 @@ function CTA(){
     return (
         <div className="cta">
             <h2>Step back in time and discover the stories hidden within Intramuros' walls.</h2>
-            <button>Book Now!</button>
+            <div className="button"
+            ><a href="https://intramuros.gov.ph/packages/">Book Now!</a></div>
         </div>
         )
     }
