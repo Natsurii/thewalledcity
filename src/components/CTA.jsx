da7588a0523d344afcc810a93a7f6a9b3fe3ae81
Natsurii <unatsurii@gmail.com>
2023-03-21 11:16:12 +0800

    :wrench: fix CTA box sizing

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,7 @@
 function CTA(){
 
     return (
-        <div className="cta">
+        <div className="cta ctabox">
             <h2>Step back in time and discover the stories hidden within Intramuros' walls.</h2>
             <div className="button"
             ><a href="https://intramuros.gov.ph/packages/">Book Now!</a></div>
