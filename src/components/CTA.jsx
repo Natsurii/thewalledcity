6b6b15578e1476fd5058ad929f6cf29f3aaa0e75
Natsurii <unatsurii@gmail.com>
2023-03-21 11:18:15 +0800

    :sparkles: add CTA button anim

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,10 +1,14 @@
+import { motion } from "framer-motion"
 function CTA(){
 
     return (
         <div className="cta ctabox">
             <h2>Step back in time and discover the stories hidden within Intramuros' walls.</h2>
-            <div className="button"
-            ><a href="https://intramuros.gov.ph/packages/">Book Now!</a></div>
+
+            <div.motion className="button"
+                whileHover={{ scale: 1.2 }}>
+                <a href="https://intramuros.gov.ph/packages/">Book Now!</a>
+            </div.motion>
         </div>
         )
     }
