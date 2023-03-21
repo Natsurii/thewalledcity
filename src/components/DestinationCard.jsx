d5c5d0cf4fbe6fcf4024296690746e116f287b19
Natsurii <unatsurii@gmail.com>
2023-03-21 11:10:43 +0800

    :sparkles: add destination animation

--- src/components/DestinationCard.jsx
+++ src/components/DestinationCard.jsx
@@ -1,17 +1,17 @@
-
+import { motion } from "framer-motion"
 
 function DestinationCard({destimage, destname}) {
+
     return (
-        <div className="destcard rectcard" 
-        style=
-        {
-            {
-                "--img": `url(${destimage})`
-            }
-        }
-        >
+        <motion.div className="destcard rectcard" 
+        whileHover={{
+            scale: 1.25,
+            rotate: -5
+        }}
+
+        style={{ "--img": `url(${destimage})`}}>
             <h3>{destname}</h3>
-        </div>
+        </motion.div>
     )
 }
 
