d7a5aca73843e6794ab1b4ec160bee8ab03e0a20
Natsurii <unatsurii@gmail.com>
2023-03-21 10:18:33 +0800

    :sparkles: Add hero animations

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { motion } from 'framer-motion';
 import fort from '/assets/fortvec.svg'
 
 function Hero(){
@@ -5,11 +6,36 @@ function Hero(){
     return (
         <div className="hero sidepad">
             <div className="hero__text">
-                <h1>The Walled City</h1>
-                <p>Experience the magic of the historic walled city of Intramuros, where the past meets the present.</p>
-                <div className="button"><a href="https://intramuros.gov.ph/guides/">Take a tour</a></div>
+                <motion.h1
+                initial={{ x: -1500 }}
+                animate={{ x: 0, y:0 }}
+                transition={{ ease: "easeOut", duration: 1.5 }}>
+                    The Walled City
+                </motion.h1>
+                
+                <motion.p
+                initial = {{ opacity:0 }}
+                animate = {{ opacity: 1 }}
+                transition = {{ ease: "easeOut", duration: 5 }}>
+                    Experience the magic of the historic walled city of Intramuros, where the past meets the present.
+                </motion.p>
+
+                <motion.div className="button"
+                 whileHover={{ scale: 1.2 }}>
+                    <a href="https://intramuros.gov.ph/guides/">Take a tour</a>
+                </motion.div>
             </div>
-            <img src={fort} alt="Fort Santiago Vector" />
+
+            <motion.img 
+            animate =
+                {{
+                    x: [0, 150, -150, 0],
+                    scale: [0, 1.25, 1.25, 1]
+                }}
+
+            transition = {{ type: "spring", stiffness: 150 }}
+           
+            src={fort} alt="Fort Santiago Vector" />
 
         </div>
     )
